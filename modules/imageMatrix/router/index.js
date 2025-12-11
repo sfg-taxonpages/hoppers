6835481a5c34dc8af3e06e06714126d61a70c8b3
jlpereira <pepelui@outlook.com>
2025-12-11 17:37:32 -0300

    Fix async import

--- modules/imageMatrix/router/index.js
+++ modules/imageMatrix/router/index.js
@@ -2,6 +2,6 @@ export default [
   {
     name: 'image-matrix-id',
     path: '/image_matrices/:id',
-    component: import('../views/id.vue')
+    component: () => import('../views/id.vue')
   }
 ]
