303cd757db823f251d23609a552bd3146f178f7e
jlpereira <pepelui@outlook.com>
2026-02-23 19:02:29 -0300

    Add image matrix panel

---
+++ panels/PanelImageMatrix/PanelImageMatrix.vue
@@ -0,0 +1,73 @@
+<template>
+  <VCard>
+    <VCardHeader>
+      <h3>Image matrix</h3>
+    </VCardHeader>
+    <VCardContent>
+      <VSpinner
+        v-if="isLoading"
+        logo-class="w-8 h-8"
+        legend=""
+      />
+      <RouterLink
+        v-if="otuIds.length"
+        class="text-sm"
+        :to="{
+          name: 'image-matrices-id',
+          params: {
+            id: 0
+          },
+          query: {
+            otu_filter: otuIds.join('|')
+          }
+        }"
+        >View descendants image matrix</RouterLink
+      >
+      <span
+        class="text-sm"
+        v-else
+      >
+        No descendants observations found
+      </span>
+    </VCardContent>
+  </VCard>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { makeAPIRequest } from '@/utils/request.js'
+
+const props = defineProps({
+  otuId: {
+    type: Number,
+    required: true
+  }
+})
+
+const otuIds = ref([])
+const isLoading = ref(false)
+
+onMounted(async () => {
+  isLoading.value = true
+
+  try {
+    const { data } = await makeAPIRequest.get('/observations', {
+      params: {
+        otu_id: [],
+        observation_type: ['Observation::Media'],
+        descendants: true,
+        per: 2000
+      }
+    })
+
+    const ids = data
+      .filter((o) => o.observation_object_type === 'Otu')
+      .map((o) => o.observation_object_id)
+
+    otuIds.value = [...new Set(ids)]
+  } catch {
+  } finally {
+    isLoading.value = false
+  }
+})
+</script>
