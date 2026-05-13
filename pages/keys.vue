f3fdde0d360720a98b02d80173223e6c2bacc266
José Luis Pereira <pepelui@outlook.com>
2026-05-13 14:39:10 -0300

    Update padding

--- pages/keys.vue
+++ pages/keys.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto py-4">
+  <div class="container mx-auto py-6">
     <h1 class="text-4xl font-bold">Keys</h1>
     <div class="flex flex-col gap-4 mt-4">
       <ObservationMatricesTable />
